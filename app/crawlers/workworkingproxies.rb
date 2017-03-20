d402fdf6c618c60b12453c0533f01b2c756cb38c
gidpoiiohika <adacer73044210@hmail.com>
2017-03-20 16:17:29 +0200

    Fixed bugs

--- app/crawlers/workworkingproxies.rb
+++ app/crawlers/workworkingproxies.rb
@@ -1,13 +1,13 @@
 class Workworkingproxies
   # http://www.workingproxies.org/?page=39
   # http://www.workingproxies.org/?page=0
-  LINK = 'http://workingproxies.org/?page='
+  LINK = 'http://workingproxies.org/?page=0'
 
   def parse
     result = []
 
     (0..39).each do |n|
-      doc = Nokogiri::HTML(open("#{LINK}#{n*30}"))
+      doc = Nokogiri::HTML(open("#{LINK}#{n*1}"))
       doc.css('tbody tr').each do |node|
         result << node.children.children.first.text + ':' + node.children.children[1].text
       end
