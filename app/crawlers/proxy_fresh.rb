79354e8ead06066f2cb5ceb5df6bb70ddfacfe2f
gidpoiiohika <adacer73044210@hmail.com>
2017-04-12 12:40:06 +0300

    add parser proxy_fresh

---
+++ app/crawlers/proxy_fresh.rb
@@ -0,0 +1,16 @@
+class ProxyFresh
+  LINK = 'http://proxy-fresh.ru/?page='
+
+  def parse
+    result = []
+
+    (0..164).each do |n|
+      doc = Nokogiri::HTML(open("#{LINK}#{n}"))
+      doc.css('tbody tr').each do |node|
+        result << node.children[3].children.text + ':' + node.children[5].text
+      end
+    end
+
+    result
+  end
+end
