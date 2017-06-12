083c056bb5e135c370370a1781906c8fd4e73030
gidpoiiohika <adacer73044210@hmail.com>
2017-06-12 12:53:24 +0300

    add

---
+++ app/crawlers/freeproxy.rb
@@ -0,0 +1,29 @@
+class Freeproxy
+
+  LINKS = [  
+    #'https://2freeproxy.com/elite-proxy.html' [4].text.squish.split(' ') 
+    'https://2freeproxy.com/anonymous-proxy.html',
+    #'https://2freeproxy.com/socks-proxy.html', [4].text.squish.split(' ')
+    'https://2freeproxy.com/socks4.html',
+    'https://2freeproxy.com/socks5.html',
+    'https://2freeproxy.com/https-proxy.html',
+    'https://2freeproxy.com/spoiled-proxy.html',
+    'https://2freeproxy.com/standart-ports-proxy.html',
+    'https://2freeproxy.com/us-proxy.html',
+    'https://2freeproxy.com/uk-proxy.html',
+    'https://2freeproxy.com/canada-proxy.html',
+    'https://2freeproxy.com/france-proxy.html'
+  ]
+
+  def parse
+    result = []
+
+    LINKS.each do |link|
+      doc = Nokogiri::HTML(open("#{link}"))
+      result = doc.css('p')[3].text.squish.split(' ')
+    end
+
+    result.uniq!
+    result
+  end
+end
