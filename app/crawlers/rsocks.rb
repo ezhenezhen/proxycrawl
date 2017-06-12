a4068f1bab00ad8549eb076c3092b80369b59406
Eugene Karpenko <ezhenezhen@gmail.com>
2017-06-12 17:37:42 +0300

    adding comments on how to parse the whole site for rsocks

--- app/crawlers/rsocks.rb
+++ app/crawlers/rsocks.rb
@@ -1,7 +1,7 @@
 class Rsocks
   #https://rsocks.net/freeproxy
-  #Login: adacer2012
-  #Password: adacer2012
+  #Login adacer2012
+  #Password adacer2012
   LINK = 'https://rsocks.net/freeproxy?page='
 
   def parse
@@ -18,3 +18,35 @@ class Rsocks
     result
   end
 end
+
+# class Rsocks
+#   #https://rsocks.net/freeproxy
+#   #Login: adacer2012
+#   #Password: adacer2012
+#   LINK = 'https://rsocks.net/freeproxy?page='
+
+#   def parse
+#     result = []
+
+#     browser = Watir::Browser.new :chrome
+#     browser.goto(LINK)
+#     sleep(60)
+# #    enter captcha and enter login here
+
+#     (0..46).each do |n|
+#       browser.goto("#{LINK}#{n}")
+     
+#       html = browser.html
+#       doc = Nokogiri::HTML(html)
+
+#       doc.css('table tr').each do |node|
+#         result << node.children[1].text + ':' + node.children[3].text
+#       end      
+#     end
+
+#       browser.close
+
+#     result.uniq!
+#     result
+#   end
+# end
