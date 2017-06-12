b22af4c780e317014e9f567914cae06892837411
gidpoiiohika <adacer73044210@hmail.com>
2017-06-12 11:48:15 +0300

    add parser rsocks.rb

---
+++ app/crawlers/rsocks.rb
@@ -0,0 +1,21 @@
+class Rsocks
+  #https://rsocks.net/freeproxy
+  #На сайте требуеться регестрация что бы получить больше прокси. 
+  #Login adacer2012
+  #Password adacer2012
+  LINK = 'https://rsocks.net/freeproxy?page='
+
+  def parse
+    result = []
+
+    (0..46).each do |n|
+      doc = Nokogiri::HTML(open("#{LINK}#{n}"))
+      doc.css('table tr').each do |node|
+        result << node.children[1].text + ':' + node.children[3].text
+      end
+    end
+    
+    result.uniq!
+    result
+  end
+end
